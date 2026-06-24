2cbeab6ec043cdab023381eaac20f37f1bb26d98
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2026-06-24 09:55:29 +0800

    fix Supabase project URL for auth

--- lib/constants/supabase_config.dart
+++ lib/constants/supabase_config.dart
@@ -1,7 +1,9 @@
 class SupabaseConfig {
-  static const url = String.fromEnvironment(
+  static final url = _normalizeUrl(
+    const String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: 'https://kfoilzgakhuzaerwmhik.supabase.co/rest/v1/',
+      defaultValue: 'https://kfoilzgakhuzaerwmhik.supabase.co',
+    ),
   );
 
   static const anonKey = String.fromEnvironment(
@@ -9,4 +11,11 @@ class SupabaseConfig {
     defaultValue:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmb2lsemdha2h1emFlcndtaGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxOTAwMTksImV4cCI6MjA5Nzc2NjAxOX0.aQhfTJcLVxPCSQitqsgDNIsCwcG0Qx-Sck-5h0hcDJU',
   );
+
+  static String _normalizeUrl(String value) {
+    return value
+        .trim()
+        .replaceFirst(RegExp(r'/rest/v1/?$'), '')
+        .replaceFirst(RegExp(r'/$'), '');
+  }
 }
